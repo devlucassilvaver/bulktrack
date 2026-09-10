782ab9f5f97c33efae0ee4e42757ba4cb2faa1ce
LK <lorenakampslk@gmail.com>
2026-09-10 08:41:24 -0300

    feat: adiciona endpoint de cadastro de perfil

--- bulktrack/src/main/java/bulktrack/controller/PerfilController.java
+++ bulktrack/src/main/java/bulktrack/controller/PerfilController.java
@@ -1,6 +1,13 @@
 package bulktrack.controller;
 
+import bulktrack.dto.PerfilRequest;
+import bulktrack.dto.PerfilResponse;
 import bulktrack.service.PerfilService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +18,15 @@ public class PerfilController {
 
     public PerfilController(PerfilService perfilService){
         this.perfilService = perfilService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PerfilResponse> criarPerfil(
+            @Valid @RequestBody PerfilRequest perfilRequest){
+        PerfilResponse perfil = perfilService.criarPerfil(perfilRequest);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(perfil);
     }
 }
